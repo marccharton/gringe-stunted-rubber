d31e46451bfe46995fca2fc7e83a008ed0bea289
narcomarch <kramoogle@gmail.com>
2020-01-30 13:09:45 +0100

    feat: add pitch on sound

--- 01.janvier - collective heap/sketch.js
+++ 01.janvier - collective heap/sketch.js
@@ -1,73 +1,97 @@
 var ch = {
-    mainColor : 0,
-    voiceList : [],
-    lastCircleCount : 0,
-    gap : 50,
-    sound: null
+	mainColor: 0,
+	voiceList: [],
+	lastCircleCount: 0,
+	gap: 50,
+	sound: null
 };
 
 function preload() {
-    ch.sound = loadSound("assets/Spooky Singing Choir.wav");
+	ch.sound = loadSound('assets/Spooky Singing Choir.wav');
 }
 
-function setup(){
-    createCanvas(windowWidth, windowHeight);
-    ch.mainColor = color(255, 204, 0);
-    ch.sound.setVolume(1);
-    ch.sound.play();
-    noFill();
-}
-
-function draw(){
-    refreshVoiceList();
-    updateVoiceList();
-    background(ch.mainColor);
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	
     strokeWeight(1);
-    drawVoiceList();
+	noFill();
+
+	ch.mainColor = color(255, 204, 0);
+	ch.sound.setVolume(1);
+    ch.sound.playMode("restart");
+	ch.sound.play();
 }
 
-function drawVoiceList() {
-    ch.voiceList.forEach(voiceItem => {
-        circle(mouseX + voiceItem.offsetX, mouseY + voiceItem.offsetY, voiceItem.size);
-    });
+function mouseClicked() {
+ if (getAudioContext().state !== 'running') {
+		getAudioContext().resume();
+	}
 }
 
-function updateVoiceList() {
-    // ch.voiceList.forEach(voiceItem => {
-    // });
+function draw() {
+    background(ch.mainColor);
+
+	refreshVoiceList();
+	updateVoiceList();
+	drawVoiceList();
 }
 
 function refreshVoiceList() {
-    let circleCount = int(map(mouseX, 0, windowWidth, 0, 10)) + 1;
-    let length = ch.voiceList.length;
+	let circleCount = int(map(mouseX, 0, windowWidth, 0, 10)) + 1;
+	let length = ch.voiceList.length;
 
-    if (ch.lastCircleCount == circleCount) { return; }
-    ch.lastCircleCount = circleCount;
+	if (ch.lastCircleCount == circleCount) {
+		return;
+	}
+	ch.lastCircleCount = circleCount;
 
-    if (circleCount > length) {
-        addVoice(length);
-    }
-    else {
-        ch.voiceList.splice(length - 1, 1)
-    }
+	if (circleCount > length) {
+		addVoice(length);
+	} else {
+		ch.voiceList.splice(length - 1, 1);
+	}
 
-    console.log(ch.voiceList);
+	console.log(ch.voiceList);
 }
 
 function addVoice(length) {
-    let offsetAmount = int(map(mouseY, 0, windowHeight, 0, 100));
-    ch.voiceList.push(new Voice(ch.mainColor, 
-                                ch.gap * length + ch.gap, 
-                                random(-offsetAmount/2, offsetAmount/2),
-                                random(-offsetAmount/2, offsetAmount/2)
-                    ));
+	let offsetAmount = int(map(mouseY, 0, windowHeight, 0, 100));
+	ch.voiceList.push(
+		new Voice(
+			ch.mainColor,
+			ch.gap * length + ch.gap,
+			random(-offsetAmount / 2, offsetAmount / 2),
+			random(-offsetAmount / 2, offsetAmount / 2)
+		)
+	);
+}
+
+function updateVoiceList() {
+	ch.voiceList.forEach(voiceItem => {
+        voiceItem.updatePitch();
+	});
+}
+
+function drawVoiceList() {
+    if (ch.voiceList.length > 0) {
+        ch.sound.rate(ch.voiceList[0].speed);
+    }
+	ch.voiceList.forEach((voiceItem) => {
+        circle(mouseX + voiceItem.offsetX, mouseY + voiceItem.offsetY, voiceItem.size);
+	});
 }
 
 class Voice {
-    constructor(color, size, offsetX, offsetY){
-        this.color = color;
-        this.size = size;
-        this.offsetX = offsetX;
-        this.offsetY = offsetY;
+	constructor(color, size, offsetX, offsetY) {
+		this.color = color;
+		this.size = size;
+		this.offsetX = offsetX;
+		this.offsetY = offsetY;
+        this.speed = 1;
+	}
+
+    updatePitch() {
+        let speed = map(mouseY, 0.1, height, 0, 2);
+        this.speed = constrain(speed, 0.01, 4);
     }
 }
